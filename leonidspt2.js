6845a1022512d9d82f8992ea8f53fa572889573e
madison-hepner <hepnermadison@icloud.com>
2021-11-01 23:04:25 -0500

    got the user input to work

---
+++ leonidspt2.js
@@ -0,0 +1,38 @@
+const toys = [
+    {
+        id: 1,
+        name: "toy 1",
+        price: 45
+    }
+    ,
+    {
+        id: 2,
+        name: "toy 2",
+        price: 65
+    }
+    ,
+    {
+        id: 3,
+        name: "toy 3",
+        price: 75
+    }
+    ,
+    {
+        id: 4,
+        name: "toy 4",
+        price: 25
+    }
+]
+
+
+
+for (const toy of toys) {
+    const toyToFind = prompt("What's the id of the phone you'd like?");
+    if (toy.id === toyToFind) {
+        toy.price = toy.price / 100 * 5 + toy.price
+        console.log(`${toy.name} costs $${toy.price}`);
+    }
+    alert(`${toy.name} costs $${toy.price}`);
+}
+
+
